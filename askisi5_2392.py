d520d1c3037c1ee91dfe04e6f78b24605d16f0eb
thanasis xan <36565555+thanasisxan@users.noreply.github.com>
2020-01-10 03:33:32 +0200

    polyonimiki

--- askisi5_2392.py
+++ askisi5_2392.py
@@ -18,18 +18,19 @@ y_sin_points = [0.371662455660328, 0.690079011482112, 0.984807753012208, 0.45822
 # Η μορφη της επιστρεφόμενης μεταβλητής είναι: numpy.poly1d
 # όπου αποτελεί δομή δεδομένων που προσομοιάζει τα πολυώνυμα
 def polyonimiki_prosegisi_newton(x_points, y_points):
-    # πλήθος των σημείων
     assert (len(x_points) == len(y_points)), "Το πλήθος των σημείων πρέπει να είναι ίδιο."
-    k = len(x_points)
-    # Το πολυώνυμο 1x+0
+
+    # πλήθος των σημείων k
+    n = len(x_points)
+
+    # Το πολυώνυμο 1x+0 (μεταβλητή x σε συμβατή μορφη)
     x = numpy.poly1d([1, 0])
+
     # Διαιρεμένες διαφορές υπολογισμένες στον πίνακα Dij
     Dij = coef(x_points, y_points)
 
     N = 0
-    for j in range(0, k):
-        # print("j", j)
-        # D1i=y[]
+    for j in range(0, n):
         nj = 1
         for i in range(0, j):
             nj = numpy.polymul(numpy.polyadd(x, -x_points[i]), nj)
@@ -44,9 +45,7 @@ def coef(x, y):
         a.append(y[i])
 
     for j in range(1, n):
-
         for i in range(n - 1, j - 1, -1):
-            # a[i] = float(a[i] - a[i - 1]) / float(x[i] - x[i - j])
             a[i] = (a[i] - a[i - 1]) / (x[i] - x[i - j])
 
     return numpy.array(a)
@@ -55,9 +54,3 @@ def coef(x, y):
 print(polyonimiki_prosegisi_newton(x_sin_points, y_sin_points)(1))
 print(sin(1))
 
-# p1 = numpy.poly1d([3, 2, 3])
-# p2 = numpy.poly1d([2, 4, 19])
-# print(numpy.polyadd(p1, p2))
-
-# plt.plot(p1)
-# plt.show()
