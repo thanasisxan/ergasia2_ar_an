c3033a5b515fd1bfdf91151ab55e3dd1a2d5ff02
thanasis xan <36565555+thanasisxan@users.noreply.github.com>
2020-01-12 22:44:29 +0200

    least square method ass

--- askisi5_2392.py
+++ askisi5_2392.py
@@ -5,18 +5,22 @@ sin = numpy.sin
 pi = numpy.pi
 
 # Τα σημεία που επέλεξα από την συναρτηση του ημιτόνου
-x_sin_points = [0.380799109526036, 0.761598219052071, 1.39626340159546, 2.66559376668225,
-                2.91945983969961, 3.2367924309713, 3.80799109526036,
-                5.20425449685582, 5.83891967939921, 6.09278575241657]
+x_my_sin_points = [0.380799109526036, 0.761598219052071, 1.39626340159546, 2.66559376668225,
+                   2.91945983969961, 3.2367924309713, 3.80799109526036,
+                   5.20425449685582, 5.83891967939921, 6.09278575241657]
 
-y_sin_points = [sin(i) for i in x_sin_points]
+y_my_sin_points = [sin(i) for i in x_my_sin_points]
+
+# Τα 200 δοκιμαστικά σημεία
+x_test_points = numpy.linspace(-pi, pi, 200)
+y_test_points = [sin(i) for i in x_test_points]
 
 
-# Επιστρέφει το πολυώνυμο(προσσέγγιση της συναρτησης) που περνάει απο τα σημέια που περνει σαν όρισμα
-# με την μέθοδο του Newton
-# Η μορφη της επιστρεφόμενης μεταβλητής είναι: numpy.poly1d
-# όπου αποτελεί δομή δεδομένων που προσομοιάζει τα πολυώνυμα
 def polyonimiki_prosegisi_newton(x_points, y_points):
+    # Επιστρέφει το πολυώνυμο(προσσέγγιση της συναρτησης) που περνάει απο τα σημέια που περνει σαν όρισμα
+    # με την μέθοδο του Newton
+    # Η μορφη της επιστρεφόμενης μεταβλητής είναι: numpy.poly1d
+    # όπου αποτελεί δομή δεδομένων που προσομοιάζει τα πολυώνυμα
     assert (len(x_points) == len(y_points)
             ), "Το πλήθος των σημείων πρέπει να είναι ίδιο."
 
@@ -38,8 +42,8 @@ def polyonimiki_prosegisi_newton(x_points, y_points):
     return N
 
 
-# συνάρτηση για τον υπολογισμό των συντελεστών α
 def coef(x, y):
+    # συνάρτηση για τον υπολογισμό των συντελεστών α για την πολυωνυμική προσσέγιση Newton
     n = len(x)
     a = []
     for i in range(n):
@@ -52,58 +56,69 @@ def coef(x, y):
     return numpy.array(a)
 
 
-polyonimo_newton = polyonimiki_prosegisi_newton(x_sin_points, y_sin_points)
+def methodos_elaxistwn_tetragwnwn(x_points, y_points):
+    # προσέγγιση των σημειών με πολυώνυμο 1ου βαθμού
+    # με την μέθοδο των ελαχίστων τετραγώνων
+    x_meso = numpy.mean(x_points)
+    y_meso = numpy.mean(y_points)
+
+    sum_arith = 0
+    sum_paron = 0
+    for i in range(len(x_points)):
+        # Δημιουργούμε το άθροισμα του αριθμητή
+        sum_arith += (x_points[i] - x_meso)*(y_points[i] - y_meso)
+        # Δημιουργούμε το άθροισμα του παρονομαστή
+        sum_paron += (x_points[i] - x_meso)**2
+    a = sum_arith / sum_paron
+    b = y_meso - a*x_meso
+    eutheia_prοseggisis = numpy.poly1d([a, b])
+    return eutheia_prοseggisis
+
+
+polyonimo_newton = polyonimiki_prosegisi_newton(
+    x_my_sin_points, y_my_sin_points)
 # print(polyonimo_newton)
 # print(polyonimo_newton(pi/2))
 # print(sin(pi/2))
-points = numpy.linspace(-pi, pi, 200)
-sum_sfalma = 0
-for point in points:
-    interp_result = polyonimo_newton(point)
-    # print(interp_result)
+
+
+elax_tetr_eutheia = methodos_elaxistwn_tetragwnwn(
+    x_my_sin_points, y_my_sin_points)
+
+# z = numpy.polyfit(x_my_sin_points, y_my_sin_points, 1)
+# print(a, b)
+# print(elax_tetr_eutheia)
+# print(z)
+
+
+# Υπολογισμός του σφάλματος για 200 σημεια μεταξύ -π και π
+sum_sfalma_newton = 0
+sum_sfalma_leastsq = 0
+for point in x_test_points:
+    interp_newton = polyonimo_newton(point)
+    interp_leastsq = elax_tetr_eutheia(point)
+    # print(interp_newton)
+    # print(interp_leastsq)
     result = sin(point)
     # print(result)
-    sfalma = abs(interp_result - result)
-    # print("Σφάλμα προσέγγισης:", sfalma)
-    sum_sfalma = sum_sfalma + sfalma
+    sfalma_newton = abs(interp_newton - result)
+    sfalma_leastsq = (interp_leastsq - result)**2
 
-avg_sfalma = sum_sfalma / len(points)
+    sum_sfalma_newton = sum_sfalma_newton + sfalma_newton
+    sum_sfalma_leastsq = sum_sfalma_leastsq + sfalma_leastsq
+
+avg_sfalma_newton = sum_sfalma_newton / len(x_test_points)
+final_sfalma_leastsq = numpy.sqrt(sum_sfalma_leastsq)
+
+print("Μέσο σφάλμα πολυωνυμικής προσσέγισης Newton:", avg_sfalma_newton)
+print("Σφάλμα προσσέγισης με την μέθοδο των ελάχιστων τετραγώνων:",
+      final_sfalma_leastsq)
 
 
-print("Μέσο σφάλμα πολυωνυμικής προσσέγισης Newton:", avg_sfalma)
+# print(Y_pred)
 
-
-plt.rcParams['figure.figsize'] = (12.0, 9.0)
-
-# Preprocessing Input data
-# data = pd.read_csv('data.csv')
-# data = [[i, sin(i)] for i in x_sin_points]
-
-X = x_sin_points
-Y = y_sin_points
-
-# Building the model
-X_mean = numpy.mean(X)
-Y_mean = numpy.mean(Y)
-
-num = 0
-den = 0
-for i in range(len(X)):
-    num += (X[i] - X_mean)*(Y[i] - Y_mean)
-    den += (X[i] - X_mean)**2
-m = num / den
-c = Y_mean - m*X_mean
-
-print(m, c)
-
-Y_pred = []
-# Making predictions
-for i in range(len(X)):
-    Y_pred.append(m*X[i]+c)
-# Y_pred = m*X + c
-
-plt.plot(X, Y)  # actual
-plt.plot(X, Y_pred, color='red')
-# plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)],
-#  color='red')  # predicted
-plt.show()
+# plt.scatter(x_test_points, y_test_points)  # actual
+# plt.scatter(x_test_points, Y_pred, color='red')
+# # plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)],
+# #  color='red')  # predicted
+# plt.show()
