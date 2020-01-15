1c836debf6865b0acf7d32b8d8478933070fcf8f
thanasis xan <36565555+thanasisxan@users.noreply.github.com>
2020-01-16 00:25:11 +0200

    custom simpson method

--- askisi6_2392.py
+++ askisi6_2392.py
@@ -41,7 +41,54 @@ def custom_olokliroma_simpson(func, a, b, points):
     return E
 
 
-print(olokliroma_simpson(sin, 0, pi / 2, 12))
+def olokliroma_trapezio(func, a, b, N):
+    sumT = 0  # το άθροισμα μέχρι Ν-1
+
+    def x_i(k):  # υπολογισμός του όρου Xi=X0+κ*(b-a)/N όπου X0=a και κ=0,...,Ν
+        return a + k * (b - a) / N
+
+    for i in range(1, N):
+        # υπολογισμών του αθροίσματος
+        sumT += func(x_i(i))
+    # το τελευταίο στοιχείο που πρέπει να προστεθεί στο άθροισμα που παει μέχρι Ν/2
+
+    E = ((b - a) / (2 * N)) * (func(x_i(0)) + func(x_i(N)) + 2 * sumT)
+    return E
+
+
+def custom_olokliroma_trapezio(func, a, b, points):
+    sumT = 0  # το άθροισμα μέχρι Ν-1
+    N = len(points) - 1
+
+    def x_i(k):  # επιστρέφει τα σημεία που εμείς επιλέξαμε για να γίνει η μέθοδος του τραπεζίου
+        return points[k]
+
+    for i in range(1, N):
+        # υπολογισμών του αθροίσματος
+        sumT += func(x_i(i))
+    # το τελευταίο στοιχείο που πρέπει να προστεθεί στο άθροισμα που παει μέχρι Ν/2
+
+    E = ((b - a) / (2 * N)) * (func(x_i(0)) + func(x_i(N)) + 2 * sumT)
+    return E
+
+
+print("Υπολογισμός με την μέθοδο Simpson (χωρίς δικά μας σημεία)", olokliroma_simpson(sin, 0, pi / 2, 10))
+print("Υπολογισμός με την μέθοδο του τραπεζίου (χωρίς δικά μας σημεία)", olokliroma_trapezio(sin, 0, pi / 2, 10))
 mypoints = [0.0158666295635848, 0.237999443453772, 0.333199220835281, 0.571198664289053, 0.650531812106977,
             0.809198107742826, 0.951997773815089, 1.12653069901452, 1.33279688334112, 1.38039677203188, 1.4597299198498]
-print(custom_olokliroma_simpson(sin, 0, pi / 2, mypoints))
+print("Υπολογισμός με την μέθοδο Simpson (με δικά μας σημεία)", custom_olokliroma_simpson(sin, 0, pi / 2, mypoints))
+print("Υπολογισμός με την μέθοδο του τραπεζίου (με δικά μας σημεία)",
+      custom_olokliroma_trapezio(sin, 0, pi / 2, mypoints))
+
+# Υπολογισμός του σφάλματος
+a = 0
+b = pi / 2
+N = 12
+# Η τέταρτη παράγωγος του ημιτόνου ειναι η συνάρτηση του ημιτόνου
+# όπου στο πεδίο [0,π/2] εχει μεγιστη τιμη το 1 για π/2
+sfalma_trapezio = ((b - a) ** 3) / (12 * N ** 2)
+sfalma_simpson = ((b - a) ** 5) / (180 * N ** 4)
+
+print()
+print("Θεωρητικό σφάλμα της μεθόδου τραπεζίου:", sfalma_trapezio)
+print("Θεωρητικό σφάλμα της μεθόδου Simpson:", sfalma_simpson)
